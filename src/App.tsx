c1a88c5315bdd26b2959895550854c59b62c257c
awwwdde <sjuu1337@gmail.com>
2026-03-14 23:56:17 +0300

    custom scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import Portfolio from './pages/portfolio.tsx'
 import Project from './pages/project.tsx'
 import Booking from './pages/booking.tsx'
 import Main from './pages/main.tsx'
+import Scrollbar from './components/scrollbar/Scrollbar.tsx'
 
 function App() {
   return (
     <div className="min-h-screen bg-black text-white">
       <Header />
-
+      <Scrollbar />
       <main>
         <Routes>
           <Route path="/" element={<Main />} />
